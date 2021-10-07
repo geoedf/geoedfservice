06992d5326334d468b2d101f8add35f3c0e500d3
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2021-10-06 20:52:09 -0400

    added permission fix to cmd file

--- runservice.sh
+++ runservice.sh
@@ -5,6 +5,11 @@ source /home/geoedf/bosco/bosco_setenv
 
 bosco_start
 
+# change ownership
+sudo chown -R geoedf:geoedf /home/geoedf/.ssh
+
+sudo chown -R geoedf:geoedf /home/geoedf/.bosco
+
 # run ssh-keyscan to enter host keys for target cluster
 
 ssh-keyscan -H brown.rcac.purdue.edu >> /home/geoedf/.ssh/known_hosts
